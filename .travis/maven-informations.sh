7234ac35fbf879e2ea61dddca2433af83dc95f4f
famaridon <famaridon@users.noreply.github.com>
2019-08-20 14:54:56 +0200

    only install

--- .travis/maven-informations.sh
+++ .travis/maven-informations.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo -ne '                     (00%)\r'
 export PROJECT_ARTIFACT_ID=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.artifactId -q -DforceStdout)
-echo -ne '###########             (50%)\r'
 export PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 
 echo "*********************************"
 echo "project.artifactId : ${PROJECT_ARTIFACT_ID}"
