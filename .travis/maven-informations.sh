836a225593653e5c3c14e350444ee9734f9ca7d1
famaridon <famaridon@users.noreply.github.com>
2019-08-20 16:02:57 +0200

    protect non buildable branch

--- .travis/maven-informations.sh
+++ .travis/maven-informations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.artifactId
+mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version
+
 PROJECT_ARTIFACT_ID=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.artifactId -q -DforceStdout)
 PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 
