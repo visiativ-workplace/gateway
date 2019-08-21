d2ef13e46bb26f44aae900ec3261f78b1aa1631b
famaridon <famaridon@users.noreply.github.com>
2019-08-21 09:06:48 +0200

    try to add debug informations

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,11 @@
 
 rm ~/.docker/config.json
 
+echo "start docker login"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin dregistry.devops.moovapps.com
+echo "end docker login"
+cat ~/.docker/config.json
+echo "end docker login"
 
 docker push dregistry.devops.moovapps.com/visiativ/process/${PROJECT_ARTIFACT_ID}:${PROJECT_VERSION}
 docker push dregistry.devops.moovapps.com/visiativ/process/${PROJECT_ARTIFACT_ID}:$1
